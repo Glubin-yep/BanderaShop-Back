51d0e3eb780c9c941a7dcc42f0095c07a84f9dbc
Dmytro Chyr <dima543439@gmail.com>
2023-03-22 00:16:48 +0200

    Add auth func, fix bug

--- service/mail-service.js
+++ service/mail-service.js
@@ -1,6 +1,6 @@
 class MailService {
     async sendActivationMail(to, link){
-
+        
     }
 
 }
--- service/user-service.js
+++ service/user-service.js
@@ -2,17 +2,32 @@ const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
+const tokenService = require('../service/token-service');
+const userDTO = require('../dtos/user-dtos');
 
 class UserService {
     async registration(email, password) {
         const candidate = await UserModel.findOne({ email })
+
         if (candidate) {
             throw new Error(`Користувач з електроною почтою ${email} вже існує`)
         }
+
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
-        const user = await UserModel.create({ email, hashPassword, activationLink })
+
+        const user = await UserModel.create({ email, password: hashPassword, activationLink })
         await mailService.sendActivationMail(email, activationLink);
+
+        const userDto = new userDTO(user);
+        const tokens = tokenService.generateTokens({ ...userDto });
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        }
     }
 
 }

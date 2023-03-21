d485b5ef6a15ba2695bc9adf21ca9cd566e87aad
Dmytro Chyr <dima543439@gmail.com>
2023-03-22 01:00:09 +0200

    Add fun for send activation mail

--- service/user-service.js
+++ service/user-service.js
@@ -17,7 +17,7 @@ class UserService {
         const activationLink = uuid.v4();
 
         const user = await UserModel.create({ email, password: hashPassword, activationLink })
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
 
         const userDto = new userDTO(user);
         const tokens = tokenService.generateTokens({ ...userDto });

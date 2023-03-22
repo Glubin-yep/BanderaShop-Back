e9a9c36f5d4f3bb48bf8647c7a164a6503c64ba8
Dmytro Chyr <dima543439@gmail.com>
2023-03-22 23:04:22 +0200

    Add middleware that verifies the token

--- service/user-service.js
+++ service/user-service.js
@@ -81,6 +81,11 @@ class UserService {
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
         return { ...tokens, user: userDto }
     }
+
+    async getAllUsers(){
+        const users = await UserModel.find();
+        return users;
+    }
 }
 
 module.exports = new UserService();

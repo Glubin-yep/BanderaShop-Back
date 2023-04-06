81599e43463ca7e5bf7a14dca00528efc7eceb17
Dmytro Chyr <dima543439@gmail.com>
2023-04-07 00:20:33 +0300

    Its work :0

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -75,8 +75,12 @@ class UserController {
             const { refreshToken } = req.cookies;
             const userData = await userService.refresh(refreshToken);
 
-            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
-
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            });
             return res.json(userData);
         }
         catch (e) {
